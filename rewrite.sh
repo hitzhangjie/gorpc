968e92b2f6c2d401d38a143af77a924f1428f55f
hitzhangjie <hit.zhangjie@gmail.com>
2019-08-03 00:32:25 +0800

    rewrite commit history

---
+++ rewrite.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="zhijiezhang@gmail.com"
+CORRECT_NAME="hitzhangjie"
+CORRECT_EMAIL="hitzhangjie@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags

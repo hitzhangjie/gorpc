655ab11541188c18368e8448a01c9613a590d0cc
hitzhangjie <hit.zhangjie@gmail.com>
2019-09-07 22:04:31 +0800

    remove redundant flagvar definition

--- tools/gorpc/main.go
+++ tools/gorpc/main.go
@@ -2,34 +2,25 @@ package main
 
 import (
 	"github.com/hitzhangjie/go-rpc/tools/gorpc/cmds"
-	"github.com/hitzhangjie/go-rpc/tools/gorpc/params"
+	"github.com/hitzhangjie/go-rpc/tools/gorpc/log"
 	"os"
 )
 
-var (
-	protodirs params.List // pb import路径
-	protofile *string     // pb 文件
-	protocol  *string     // 协议类型，如gorpc等等
-	httpon    *bool       // 是否开启http
-	assetdir  *string     // 模板路径
-	global    *bool       // 生成代码时使用全局GOPATH
-	verbose   *bool       // 打印详细日志
-)
-
-var subcmds = cmds.SubCmds()
-
 func main() {
+	subcmds := cmds.SubCmds()
 
 	if l := len(os.Args); l == 1 {
 		subcmds["help"].Run()
 		return
 	}
 
-	f, ok := subcmds[os.Args[1]]
-	if !ok || f == nil {
+	cmd, ok := subcmds[os.Args[1]]
+	if !ok || cmd == nil {
 		subcmds["help"].Run()
 		return
 	}
 
-	f.Run(os.Args[2:]...)
+	if err := cmd.Run(os.Args[2:]...); err != nil {
+		log.Error("Run command:%v error:\n\t\t%v", os.Args, err)
+	}
 }

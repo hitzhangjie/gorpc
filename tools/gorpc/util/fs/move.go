b9aeec0d00b06c3936a91ae4e8d893df0dadd491
hitzhangjie <hit.zhangjie@gmail.com>
2019-09-13 12:39:39 +0800

    doc(fs): improve documentations

--- tools/gorpc/util/fs/move.go
+++ tools/gorpc/util/fs/move.go
@@ -12,6 +12,7 @@ import (
 //
 // the behavior of fs.Move is consistent with bash shell `mv` command:
 //
+// when move a file, actions are following:
 // ------------------------------------------------------------------------------------------------
 // | No. | src existed | src type | dst existed | dst type | behavior                             |
 // ------------------------------------------------------------------------------------------------
@@ -19,17 +20,21 @@ import (
 // ------------------------------------------------------------------------------------------------
 // | 2   | True        | File     | False       | -        | if dir(dst) existed:                 |
 // |     |             |          |             |          | - Yes, is dir, mv `src` to dir(dst)  |
-// |     |             |          |             |          | - Yes, not dir, err: File Exists     |
+// |     |             |          |             |          | - Yes, not dir, err: Not a directory |
 // |     |             |          |             |          | - No, err: No such file or directory |
 // ------------------------------------------------------------------------------------------------
 // | 3   | True        | File     | True        | Folder   | if dst/basename(src) existed:        |
 // |     |             |          |             |          | - Yes, mv `src` to dst/basename(src) |
 // |     |             |          |             |          | - No, mv `src` to dst/basename(src)  |
 // ------------------------------------------------------------------------------------------------
+//
+// when move a directory, actions are following:
+// ------------------------------------------------------------------------------------------------
 // | 4   | True        | File     | True        | File     | mv `src` to dst                      |
 // ------------------------------------------------------------------------------------------------
-// | 5   | True        | Folder   | -           | -        | if dir(dst) existed:                 |
-// |     |             |          |             |          | - Yes, mv `src` to dir(dst)          |
+// | 5   | True        | Folder   | False       | -        | if dir(dst) existed:                 |
+// |     |             |          |             |          | - Yes, is dir, mv `src` to dir(dst)  |
+// |     |             |          |             |          | - Yes, not dir, err: File Exists     |
 // |     |             |          |             |          | - No, err: No such file or directory |
 // ------------------------------------------------------------------------------------------------
 // | 6   | True        | Folder   | True        | File     | error: File Already Existed          |
@@ -44,17 +49,17 @@ import (
 func Move(src, dst string) error {
 
 	var (
-		srcInf os.FileInfo
-		err    error
+		inf os.FileInfo
+		err error
 	)
 
 	// check whether `src` is valid or not
-	if srcInf, err = os.Lstat(src); err != nil {
+	if inf, err = os.Lstat(src); err != nil {
 		return err
 	}
 
 	// move directory
-	if srcInf.IsDir() {
+	if inf.IsDir() {
 		return moveDirectory(src, dst)
 	}
 
@@ -73,35 +78,35 @@ func Move(src, dst string) error {
 // - if dst is a normal file, rename src to dst
 // - if dst is a folder, rename src to dst/basename(src)
 func moveFile(src, dst string) error {
-	var (
-		dstInf os.FileInfo
-		err    error
-	)
 
-	dstInf, err = os.Lstat(dst)
-
-	// if dst not existed
-
-	if os.IsNotExist(err) {
-		p := filepath.Dir(dst)
-		if inf, err := os.Lstat(p); err != nil {
-			return err
-		} else {
-			if !inf.IsDir() {
-				return &os.PathError{"lstat", p, syscall.EEXIST}
-			}
-			return os.Rename(src, dst)
-		}
-	}
+	dstInf, err := os.Lstat(dst)
 
 	// if dst existed
 
-	if !dstInf.IsDir() {
-		return os.Rename(src, dst)
+	if err == nil {
+		if !dstInf.IsDir() {
+			return os.Rename(src, dst)
+		}
+
+		p := path.Join(dst, filepath.Base(src))
+		return os.Rename(src, p)
 	}
 
-	p := path.Join(dst, filepath.Base(src))
-	return os.Rename(src, p)
+	if !os.IsNotExist(err) {
+		return err
+	}
+
+	// if dst not existed
+
+	p := filepath.Dir(dst)
+	if inf, err := os.Lstat(p); err != nil {
+		return err
+	} else {
+		if !inf.IsDir() {
+			return &os.PathError{"lstat", p, syscall.EEXIST}
+		}
+		return os.Rename(src, dst)
+	}
 }
 
 // moveDirectory move a directory `src` to `dst`
@@ -111,27 +116,32 @@ func moveFile(src, dst string) error {
 // 	- if `dst` is not a directory, return error &PathError{"lstat", dst, os.EEXIST}
 // 	- if `dst` is a directory
 //		- if dst/basename(src) is empty, then rename src to dst/basename(src)
-//		- if dst/basename(src) not empty, return error &PathError{"mv", dst, syscall.ENOTEMPTY}
+//		- if dst/basename(src) not empty, return error &PathError{"lstat", dst, syscall.ENOTEMPTY}
 // 2. if `dst` not existed
 // 	- if dir(dst) existed, rename src to dst
-// 	- if dir(dst) not existed, return error &PathError{"mv", dst, syscall.ENOENT}
+// 	- if dir(dst) not existed, return error &PathError{"lstat", dst, syscall.ENOENT}
 func moveDirectory(src, dst string) error {
 
 	dstInf, err := os.Lstat(dst)
 
 	// if dst existed
-	if !os.IsNotExist(err) {
+	if err == nil {
 
 		if !dstInf.IsDir() {
-			return &os.PathError{"mv", dst, syscall.EEXIST}
+			return &os.PathError{"lstat", dst, syscall.EEXIST}
 		}
 
 		target := path.Join(dst, filepath.Base(src))
-		if _, err := os.Lstat(target); err != nil {
+		inf, err := os.Lstat(target)
+		if err != nil {
 			if os.IsNotExist(err) {
 				return os.Rename(src, target)
 			}
 			return err
+		}
+
+		if !inf.IsDir() {
+			return &os.PathError{"lstat", target, syscall.EEXIST}
 		}
 
 		files, err := ioutil.ReadDir(target)
@@ -139,7 +149,7 @@ func moveDirectory(src, dst string) error {
 			return err
 		}
 		if len(files) != 0 {
-			return &os.PathError{"mv", target, syscall.ENOTEMPTY}
+			return &os.PathError{"lstat", target, syscall.ENOTEMPTY}
 		}
 
 		if err = os.RemoveAll(target); err != nil {
@@ -149,12 +159,12 @@ func moveDirectory(src, dst string) error {
 	}
 
 	// if dst not existed
-	inf, err := os.Lstat(filepath.Base(dst))
+	inf, err := os.Lstat(filepath.Dir(dst))
 	if err != nil {
 		return err
 	}
 	if !inf.IsDir() {
-		return &os.PathError{"mv", filepath.Base(dst), syscall.ENOENT}
+		return &os.PathError{"lstat", filepath.Base(dst), syscall.EEXIST}
 	}
 	return os.Rename(src, dst)
 }

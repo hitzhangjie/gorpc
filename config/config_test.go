ba25a88eb35a5f3e97023777f9cf0235244e01fb
hitzhangjie <hit.zhangjie@gmail.com>
2019-10-12 22:21:27 +0800

    feat: add config implemention, including IniConfig and YamlConfig

--- config/config_test.go
+++ config/config_test.go
@@ -9,22 +9,31 @@ import (
 )
 
 var (
-	cwd    string
-	err    error
-	iniCfg *config.IniConfig
+	iniCfg  *config.IniConfig
+	yamlCfg *config.YamlConfig
 )
 
 func TestMain(m *testing.M) {
 
-	cwd, err = os.Getwd()
+	cwd, err := os.Getwd()
 	if err != nil {
 		panic(err)
 	}
 
-	iniCfg, err = config.LoadIniConfig(filepath.Join(cwd, "testdata/service.ini"))
+	// load service.ini
+	ini, err := config.LoadIniConfig(filepath.Join(cwd, "testdata/service.ini"))
 	if err != nil {
 		panic(err)
 	}
+	iniCfg = ini
+
+	// load service.yml
+	yml, err := config.LoadYamlConfig(filepath.Join(cwd, "testdata/service.yml"))
+	if err != nil {
+		panic(err)
+	}
+	yamlCfg = yml
+
 	m.Run()
 }
 
@@ -37,15 +46,15 @@ func TestIniConfig(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-		want string
+		want interface{}
 	}{
 		{"load-[]-app_mode", args{"", "app_mode", ""}, "development"},
 		{"load-[]-not_existed", args{"", "not_existed", "xxx"}, "xxx"},
 		{"load-[]=not_existed_again", args{"", "not_existed", "yyy"}, "yyy"},
 		{"load-[paths]-data", args{"paths", "data", ""}, "/home/git/grafana"},
 		{"load-[server]-protocol", args{"server", "protocol", ""}, "http"},
-		{"load-[server]-http_port", args{"server", "http_port", ""}, "9999"},
-		{"load-[server]-enforce_domain", args{"server", "enforce_domain", ""}, "true"},
+		{"load-[server]-http_port", args{"server", "http_port", 0}, 9999},
+		{"load-[server]-enforce_domain", args{"server", "enforce_domain", false}, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -54,7 +63,7 @@ func TestIniConfig(t *testing.T) {
 			case string:
 				v := iniCfg.String(tt.args.section, tt.args.property, tt.args.dftValue.(string))
 				got = v
-			case int8:
+			case int:
 				v := iniCfg.Int(tt.args.section, tt.args.property, tt.args.dftValue.(int))
 				got = v
 			case bool:
@@ -68,7 +77,7 @@ func TestIniConfig(t *testing.T) {
 	}
 }
 
-func TestIniConfig_ImplConfig(t *testing.T) {
+func TestConfig_IniConfig(t *testing.T) {
 	type args struct {
 		key      string
 		dftValue interface{}
@@ -76,15 +85,15 @@ func TestIniConfig_ImplConfig(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-		want string
+		want interface{}
 	}{
 		{"load-[]-app_mode", args{"app_mode", ""}, "development"},
 		{"load-[]-not_existed", args{"not_existed", "xxx"}, "xxx"},
 		{"load-[]=not_existed_again", args{"not_existed", "yyy"}, "yyy"},
 		{"load-[paths]-data", args{"paths.data", ""}, "/home/git/grafana"},
 		{"load-[server]-protocol", args{"server.protocol", ""}, "http"},
-		{"load-[server]-http_port", args{"server.http_port", ""}, "9999"},
-		{"load-[server]-enforce_domain", args{"server.enforce_domain", ""}, "true"},
+		{"load-[server]-http_port", args{"server.http_port", 0}, 9999},
+		{"load-[server]-enforce_domain", args{"server.enforce_domain", false}, true},
 	}
 	var cfg config.Config = iniCfg
 	for _, tt := range tests {
@@ -94,11 +103,48 @@ func TestIniConfig_ImplConfig(t *testing.T) {
 			case string:
 				v := cfg.Read(tt.args.key, tt.args.dftValue.(string))
 				got = v
-			case int8:
+			case int:
 				v := cfg.ReadInt(tt.args.key, tt.args.dftValue.(int))
 				got = v
 			case bool:
 				v := cfg.ReadBool(tt.args.key, tt.args.dftValue.(bool))
+				got = v
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("case:%s, got = %v, want = %v", tt.name, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestConfig_YamlConfig(t *testing.T) {
+	type args struct {
+		path     string
+		dftValue interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want interface{}
+	}{
+		{"load-name", args{"name", ""}, "smallfish"},
+		{"load-age", args{"age", 0}, 99},
+		{"load-bool", args{"bool", false}, true},
+		{"load-bb.cc.dd.ee", args{"bb.cc.ee", ""}, "aaa"},
+	}
+	var cfg config.Config = yamlCfg
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var got interface{}
+			switch tt.args.dftValue.(type) {
+			case string:
+				v := cfg.Read(tt.args.path, tt.args.dftValue.(string))
+				got = v
+			case int:
+				v := cfg.ReadInt(tt.args.path, tt.args.dftValue.(int))
+				got = v
+			case bool:
+				v := cfg.ReadBool(tt.args.path, tt.args.dftValue.(bool))
 				got = v
 			}
 			if !reflect.DeepEqual(got, tt.want) {
---
+++ config/config_yaml.go
@@ -0,0 +1,84 @@
+package config
+
+import (
+	"github.com/smallfish/simpleyaml"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+type YamlConfig struct {
+	yml *simpleyaml.Yaml
+}
+
+func LoadYamlConfig(filepath string) (*YamlConfig, error) {
+	fin, err := os.Open(filepath)
+	if err != nil {
+		return nil, err
+	}
+	data, err := ioutil.ReadAll(fin)
+	if err != nil {
+		return nil, err
+	}
+
+	yml, err := simpleyaml.NewYaml(data)
+	if err != nil {
+		return nil, err
+	}
+
+	return &YamlConfig{yml}, nil
+}
+
+func (c *YamlConfig) Read(key string, dftValue string) string {
+
+	if c.yml == nil {
+		return dftValue
+	}
+
+	paths := c.path(key)
+
+	v, err := c.yml.GetPath(paths...).String()
+	if err != nil || len(v) == 0 {
+		return dftValue
+	}
+	return v
+}
+
+func (c *YamlConfig) ReadInt(key string, dftValue int) int {
+
+	if c.yml == nil {
+		return dftValue
+	}
+
+	path := c.path(key)
+
+	v, err := c.yml.GetPath(path...).Int()
+	if err != nil || v == 0 {
+		return dftValue
+	}
+	return v
+}
+
+func (c *YamlConfig) ReadBool(key string, dftValue bool) bool {
+
+	if c.yml == nil {
+		return dftValue
+	}
+
+	path := c.path(key)
+
+	v, err := c.yml.GetPath(path...).Bool()
+	if err != nil || !v {
+		return dftValue
+	}
+	return v
+}
+
+func (c *YamlConfig) path(key string) []interface{} {
+	path := strings.Split(key, ".")
+	paths := make([]interface{}, len(path), len(path))
+	for idx, p := range path {
+		paths[idx] = p
+	}
+	return paths
+}

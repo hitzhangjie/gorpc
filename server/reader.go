9ad8ca02e6052ed45a0b0905faac9a0a3682fdd5
hitzhangjie <hit.zhangjie@gmail.com>
2019-10-19 21:10:00 +0800

    refactor: improve reader

--- server/reader.go
+++ server/reader.go
@@ -1,49 +1,81 @@
-package codec
+package server
 
 import (
+	"context"
+	"github.com/hitzhangjie/go-rpc/codec"
 	"net"
+	"sync"
+	"time"
 )
 
-// MessageReader read req from `net.Conn`, if read successfully, return the req's session.
+var mempool = &sync.Pool{
+	New: func() interface{} {
+		return make([]byte, 0, 64*1024)
+	},
+}
+
+// MessageReader read req from `net.Conn`, if read successfully, return the req'svr session.
 //
 // if any error occurs, it returns nil session and error, error should be one of the following:
 // - io.Timeout
 // - ...
 type MessageReader struct {
-	Codec Codec
+	Codec codec.Codec
 }
 
-func NewMessageReader(codec Codec) *MessageReader {
+func NewMessageReader(codec codec.Codec) *MessageReader {
 	r := &MessageReader{Codec: codec}
 	return r
 }
 
-func (r *MessageReader) Read(conn net.Conn) (interface{}, error) {
+func (r *MessageReader) Read(ctx context.Context, conn net.Conn, reqCh chan interface{}) error {
+
+	buf := mempool.Get().([]byte)
+
+	defer func() {
+		conn.Close()
+		mempool.Put(buf)
+		close(reqCh)
+	}()
 
 	var (
-		req interface{}
-		err error
-		n   int
-		m   int
+		buflen int
+		readsz int
+		err    error
 	)
 
 	for {
-		if m, err = conn.Read(buf[n:]); err != nil {
-			return nil, err
+
+		// check if server to be closed
+		select {
+		case <-ctx.Done():
+			return errServerCtxDone
+		default:
 		}
-		n += m
 
-		dat := buf[:n]
-
-		// decode请求
-		req, err = r.Codec.Decode(dat)
-		if err != nil {
-			if err == CodecReadIncomplete {
+		// fixme conn read deadline
+		if readsz, err = conn.Read(buf[buflen:]); err != nil {
+			// fixme check tcpconn idle & release
+			if e, ok := err.(net.Error); ok && e.Temporary() {
+				time.Sleep(time.Millisecond * 10)
 				continue
 			}
-			return nil, err
+			return err
 		}
-	}
+		buflen += readsz
 
-	return req, nil
+		// decode请求
+		req, sz, err := r.Codec.Decode(buf[0:buflen])
+		if err != nil {
+			if err == codec.CodecReadIncomplete {
+				continue
+			}
+			//return nil, err
+			return err
+		}
+
+		reqCh <- req
+		buf = buf[sz:]
+		buflen -= sz
+	}
 }

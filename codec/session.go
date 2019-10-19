854de1f2c098b4d35d0106321f5c895001841131
hitzhangjie <hit.zhangjie@gmail.com>
2019-10-20 02:08:35 +0800

    doc: add doc comments

--- codec/session.go
+++ codec/session.go
@@ -5,19 +5,32 @@ import (
 	"sync"
 )
 
+// Session defines the rpc context for a request
+//
+// Usually, session can be stored in client or server side, we adopt the term `session`
+// instead of `RpcContext` for simplicity.
 type Session interface {
+
+	// RPC return rpc name, i.e., the method name defined in pb service.rpc.name
 	RPC() string
 
+	// Request return the request header
 	Request() interface{}
+	// SetRequest set the request header
 	SetRequest(req interface{})
 
+	// Response return the response header
 	Response() interface{}
+	// SetResponse set the response header
 	SetResponse(rsp interface{})
+	// SetErrorResponse set the error response
 	SetErrorResponse(error)
 
+	// TraceContext return the tracing context
 	TraceContext() interface{}
 }
 
+// BaseSession implements some basic methods defined in `Session`
 type BaseSession struct {
 	Request  interface{}
 	Response interface{}
@@ -28,28 +41,29 @@ var (
 	builders = map[string]SessionBuilder{}
 )
 
+// SessionBuilder when extending protocols, SessionBuilder should be
+// implemented and registered to help build the `Session`.
 type SessionBuilder interface {
 	Build(reqHead interface{}) (Session, error)
 }
 
-func RegisterSessionBuilder(name string, builder SessionBuilder) {
+// RegisterSessionBuilder register extended SessionBuilder for protocol `proto`
+func RegisterSessionBuilder(proto string, builder SessionBuilder) {
 	lock.Lock()
 	defer lock.Unlock()
-	builders[name] = builder
+	builders[proto] = builder
 }
 
-func GetSessionBuilder(name string) SessionBuilder {
+// GetSessionBuilder return SessionBuilder for protocol `proto`
+func GetSessionBuilder(proto string) SessionBuilder {
 	lock.RLock()
 	defer lock.RUnlock()
-	return builders[name]
+	return builders[proto]
 }
 
 var sessionKey = "session"
 
-func SessionKey() string {
-	return sessionKey
-}
-
+// SessionFromContext return Session carried by `ctx`
 func SessionFromContext(ctx context.Context) Session {
 	v := ctx.Value(sessionKey)
 	session ,ok := v.(Session)
@@ -59,6 +73,7 @@ func SessionFromContext(ctx context.Context) Session {
 	return session
 }
 
+// ContextWithSession return new context carrying value `session`
 func ContextWithSession(ctx context.Context, session Session) context.Context {
 	return context.WithValue(ctx, sessionKey, session)
 }

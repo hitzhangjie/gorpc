7b0432d63bc0a72887a2f730cc8e953f1f766e60
hitzhangjie <hit.zhangjie@gmail.com>
2019-10-20 03:04:12 +0800

    feat: whisper protocol add field `rpcname`

---
+++ client/client_test.go
@@ -0,0 +1,27 @@
+package client
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewClient(t *testing.T) {
+	type args struct {
+		name string
+		opts []Option
+	}
+	tests := []struct {
+		name string
+		args args
+		want Client
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewClient(tt.args.name, tt.args.opts...); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewClient() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

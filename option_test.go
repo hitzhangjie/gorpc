d474cf9db132edfb1d551388f2e7a8866ba5b5f8
hitzhangjie <hit.zhangjie@gmail.com>
2020-02-22 18:46:49 +0800

    test: gorpc.WithConfig option

---
+++ option_test.go
@@ -0,0 +1,15 @@
+package gorpc
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWithConfig(t *testing.T) {
+	opts := options{}
+	o := WithConfig("../testcase/service.ini")
+	o(&opts)
+
+	assert.Equal(t, "../testcase/service.ini", opts.configfile)
+}

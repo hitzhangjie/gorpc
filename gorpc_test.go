0273d8295b3890ccca88d5dcdc87564a1c97d300
hitzhangjie <hit.zhangjie@gmail.com>
2020-02-22 18:12:26 +0800

    test: gorpc.ListenAndServe

--- gorpc_test.go
+++ gorpc_test.go
@@ -4,11 +4,13 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 
 	gorpc "github.com/hitzhangjie/go-rpc"
 	_ "github.com/hitzhangjie/go-rpc/codec/whisper"
 )
 
+// Test `gorpc.ListenAndServe`
 func TestListenAndServe(t *testing.T) {
 	dir, err := os.Getwd()
 	if err != nil {
@@ -17,4 +19,8 @@ func TestListenAndServe(t *testing.T) {
 	fp := filepath.Join(dir, "testcase/service.ini")
 
 	gorpc.ListenAndServe(gorpc.WithConfig(fp))
+
+	// Linux: run `fuser port/tcp` or `fuser port/udp` to check whether server working
+	// macOS: run `lsof -i tcp:port` or `lsof -i udp:port` to check whether server working
+	time.Sleep(time.Second * 10)
 }

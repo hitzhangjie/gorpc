f48288464ae79eca1d2353949e41f965d8eeb37d
hitzhangjie <hit.zhangjie@gmail.com>
2020-03-05 22:30:20 +0800

    remove build

--- version.go
+++ version.go
@@ -8,22 +8,20 @@ type Version struct {
 	Minor    string
 	Patch    string
 	Metadata string
-	Build    string
 }
 
-
-var (
-	// GoRPCVersion is the current version of Delve.
-	GoRPCVersion = Version{
-		Major: "0", Minor: "1", Patch: "0", Metadata: "",
-		Build: "$Id: ddcc99e754f9c96328ac82c53a4af978f71a7df3 $",
-	}
-)
+// GoRPCVersion is the current version of Delve.
+var GoRPCVersion = Version{
+	Major:    "0",
+	Minor:    "1",
+	Patch:    "0",
+	Metadata: "dev",
+}
 
 func (v Version) String() string {
 	ver := fmt.Sprintf("Version: %s.%s.%s", v.Major, v.Minor, v.Patch)
 	if v.Metadata != "" {
 		ver += "-" + v.Metadata
 	}
-	return fmt.Sprintf("%s\nBuild: %s", ver, v.Build)
+	return ver
 }

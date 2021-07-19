702901248a076d7a44c8e61ee98046d085b0c401
hitzhangjie <hit.zhangjie@gmail.com>
2021-07-19 20:49:32 +0800

    test/config: update testcases

--- config/decoder_test.go
+++ config/decoder_test.go
@@ -54,19 +54,18 @@ func TestConfig_IniConfig(t *testing.T) {
 		{"load-[server]-http_port", args{"server.http_port", 0}, 9999},
 		{"load-[server]-enforce_domain", args{"server.enforce_domain", false}, true},
 	}
-	var cfg config.Config = iniCfg
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			var got interface{}
 			switch tt.args.dftValue.(type) {
 			case string:
-				v := cfg.Read(tt.args.key, tt.args.dftValue.(string))
+				v := iniCfg.Read(tt.args.key, tt.args.dftValue.(string))
 				got = v
 			case int:
-				v := cfg.ReadInt(tt.args.key, tt.args.dftValue.(int))
+				v := iniCfg.ReadInt(tt.args.key, tt.args.dftValue.(int))
 				got = v
 			case bool:
-				v := cfg.ReadBool(tt.args.key, tt.args.dftValue.(bool))
+				v := iniCfg.ReadBool(tt.args.key, tt.args.dftValue.(bool))
 				got = v
 			}
 			if !reflect.DeepEqual(got, tt.want) {
@@ -91,19 +90,18 @@ func TestConfig_YamlConfig(t *testing.T) {
 		{"load-bool", args{"bool", false}, true},
 		{"load-bb.cc.dd.ee", args{"bb.cc.ee", ""}, "aaa"},
 	}
-	var cfg config.Config = yamlCfg
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			var got interface{}
 			switch tt.args.dftValue.(type) {
 			case string:
-				v := cfg.Read(tt.args.path, tt.args.dftValue.(string))
+				v := yamlCfg.Read(tt.args.path, tt.args.dftValue.(string))
 				got = v
 			case int:
-				v := cfg.ReadInt(tt.args.path, tt.args.dftValue.(int))
+				v := yamlCfg.ReadInt(tt.args.path, tt.args.dftValue.(int))
 				got = v
 			case bool:
-				v := cfg.ReadBool(tt.args.path, tt.args.dftValue.(bool))
+				v := yamlCfg.ReadBool(tt.args.path, tt.args.dftValue.(bool))
 				got = v
 			}
 			if !reflect.DeepEqual(got, tt.want) {

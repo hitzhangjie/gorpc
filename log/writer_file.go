e81bd5a8d642417e69fdf57c8bb402e6b874ad92
hitzhangjie <hit.zhangjie@gmail.com>
2021-07-25 20:35:03 +0800

    test/log: add testcase

--- log/writer_file.go
+++ log/writer_file.go
@@ -3,6 +3,7 @@ package log
 import (
 	"fmt"
 	"os"
+	"sync"
 	"sync/atomic"
 	"time"
 )
@@ -12,87 +13,100 @@ func init() {
 }
 
 type fileWriter struct {
+	sync.Mutex
 	opts *options
-	fout atomic.Value
+	fout *os.File
 	ch   chan []byte
-	done chan struct{}
-}
-
-func (w *fileWriter) Write(b []byte) (n int, err error) {
-	return w.fout.Load().(*os.File).Write(b)
-}
-
-func (w *fileWriter) AsyncWrite(b []byte) {
-	w.ch <- b
-}
-
-func (w *fileWriter) asyncWrite() {
-	for {
-		select {
-		case m := <-w.ch:
-			w.fout.Load().(*os.File).Write(m)
-		case <-w.done:
-			w.fout.Load().(*os.File).Close()
-			return
-		}
-	}
-}
-
-func (w *fileWriter) roll() {
-	tick := time.NewTicker(time.Second)
-
-	for range tick.C {
-		fp := w.fout.Load().(*os.File).Name()
-		inf, err := os.Lstat(fp)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "lstat err: %v", err)
-			continue
-		}
-
-		sz := inf.Size()
-		if sz < int64(w.opts.maxFileSZ) {
-			continue
-		}
-
-		// rename the files
-		os.Rename(fp, fmt.Sprintf("%s.%d", fp, sz))
-		f, err := os.OpenFile(fp, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "open file err: %v\n", err)
-			continue
-		}
-		w.fout.Store(f)
-
-		time.AfterFunc(time.Second, func() {
-			w.fout.Load().(*os.File).Close()
-		})
-	}
-}
-
-func (w *fileWriter) Flush() error {
-	return w.fout.Load().(*os.File).Sync()
-}
-
-func (w *fileWriter) Close() error {
-	return w.fout.Load().(*os.File).Close()
+	num  int64
 }
 
 func NewFileWriter(opts *options) (Writer, error) {
-	fw := &fileWriter{opts: opts}
+	fw := &fileWriter{
+		opts: opts,
+		num:  1,
+		ch:   make(chan []byte, 1024),
+	}
 
 	fout, err := os.OpenFile(opts.fpath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
 	if err != nil {
 		return nil, err
 	}
-	fw.fout.Store(fout)
+	fw.fout = fout
 
-	if fw.opts.async {
-		go fw.asyncWrite()
-	}
+	go fw.write()
 
 	if fw.opts.rollType != RollNONE {
 		go fw.roll()
 	}
 
 	return fw, nil
+}
+
+func (w *fileWriter) Write(b []byte) (n int, err error) {
+	w.ch <- b
+	return
+}
+
+func (w *fileWriter) write() {
+	for {
+		m, ok := <-w.ch
+		if !ok {
+			return
+		}
+
+		w.Lock()
+		_, err := w.fout.Write(m)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "write error: %v\n", err)
+		}
+		w.Unlock()
+	}
+}
+
+func (w *fileWriter) roll() {
+	tick := time.NewTicker(time.Millisecond * 500)
+
+	for range tick.C {
+		w.Lock()
+		fp := w.fout.Name()
+		inf, err := os.Lstat(fp)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "lstat err: %v", err)
+			w.Unlock()
+			continue
+		}
+
+		sz := inf.Size()
+		if sz < int64(w.opts.maxFileSZ) {
+			w.Unlock()
+			continue
+		}
+
+		// rename the files
+		os.Rename(fp, fmt.Sprintf("%s.%d", fp, atomic.LoadInt64(&w.num)))
+		atomic.AddInt64(&w.num, 1)
+
+		f, err := os.OpenFile(fp, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "open file err: %v\n", err)
+			w.Unlock()
+			continue
+		}
+		w.fout.Sync()
+		w.fout.Close()
+		w.fout = f
+		w.Unlock()
+	}
+}
+
+func (w *fileWriter) Flush() error {
+	w.Lock()
+	w.Unlock()
+	return w.fout.Sync()
+}
+
+func (w *fileWriter) Close() error {
+	w.Lock()
+	w.Unlock()
+	return w.fout.Close()
 }
--- log/writer_file_test.go
+++ log/writer_file_test.go
@@ -7,45 +7,76 @@ import (
 	"path/filepath"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/hitzhangjie/gorpc/log"
 )
 
-func TestNewFileWriter_SyncWriter(t *testing.T) {
-	t.Run("test sync writer", func(t *testing.T) {
-		fp := filepath.Join(os.TempDir(), "gorpc_sync_test.log")
-		testWriter(t, fp, false)
-		_ = os.RemoveAll(fp)
+type row struct {
+	level  log.Level
+	text   string
+	repeat int
+}
+
+var rows = []row{
+	{level: log.Trace, text: "this is one trace log message", repeat: 1},
+	{level: log.Debug, text: "this is one debug log message", repeat: 2},
+	{level: log.Info, text: "this is one info message", repeat: 1},
+	{level: log.Warn, text: "this is one warn message", repeat: 1},
+	{level: log.Error, text: "this is one error message", repeat: 10},
+	//{level: log.Fatal, text: "this is one fatal message", repeat: 1}, // skip this
+}
+
+func expect(rows []row) string {
+	var sb strings.Builder
+	for _, r := range rows {
+		for i := 0; i < r.repeat; i++ {
+			if r.level >= log.Debug {
+				sb.WriteString(fmt.Sprintf("[%s] %s\n", r.level, r.text))
+			}
+		}
+	}
+	return sb.String()
+}
+
+func TestNewFileWriter_Write(t *testing.T) {
+	fp := filepath.Join(os.TempDir(), "gorpc_logwrite.log")
+	testWrite(t, fp, log.RollNONE, 0, 0)
+
+	b, err := ioutil.ReadFile(fp)
+	assert.Nil(t, err)
+	assert.Equal(t, expect(rows), string(b))
+
+	_ = os.Remove(fp)
+}
+
+func TestFileWriter_Roll(t *testing.T) {
+	t.Run("roll by filesz", func(t *testing.T) {
+		fp := filepath.Join(os.TempDir(), "gorpc_logwrite_rollby_filesz.log")
+		testWrite(t, fp, log.RollByFileSZ, 16, time.Second)
+		files, err := filepath.Glob(fp + "*")
+		assert.Nil(t, err)
+		assert.NotEmpty(t, files)
+		assert.Greater(t, len(files), 1)
+		for _, f := range files {
+			_ = os.Remove(f)
+		}
 	})
 
-	t.Run("test async writer", func(t *testing.T) {
-		fp := filepath.Join(os.TempDir(), "gorpc_async_test.log")
-		testWriter(t, fp, true)
-		_ = os.RemoveAll(fp)
+	t.Run("roll by day", func(t *testing.T) {
+		fp := filepath.Join(os.TempDir(), "gorpc_logwrite_rollby_day.log")
+		testWrite(t, fp, log.RollByDay, 16, time.Second)
+		_ = os.RemoveAll(filepath.Dir(fp))
 	})
 }
 
-func TestNewFileWriter_AsyncWrite(t *testing.T) {
-
-}
-
-func TestFileWriter_RollBy_FixSize(t *testing.T) {
-
-}
-
-func TestFileWriter_RollBy_Day(t *testing.T) {
-
-}
-
-func testWriter(t *testing.T, fp string, async bool) {
+func testWrite(t *testing.T, fp string, rolltyp log.RollType, rollsz int, d time.Duration) {
 	opts := []log.Option{
 		log.WithWriteType(log.FileWriter),
-		log.WithRollType(log.RollNONE),
-	}
-	if !async {
-		opts = append(opts, log.WithAsyncWrite(false))
+		log.WithRollType(rolltyp),
+		log.WithMaxFileSZ(rollsz),
 	}
 
 	l, err := log.NewLogger(fp, log.Debug, opts...)
@@ -53,27 +84,10 @@ func testWriter(t *testing.T, fp string, async bool) {
 	assert.NotNil(t, l)
 	assert.FileExists(t, fp)
 
-	type row struct {
-		level  log.Level
-		text   string
-		repeat int
-	}
-	rows := []row{
-		{level: log.Trace, text: "this is one trace log message", repeat: 1},
-		{level: log.Debug, text: "this is one debug log message", repeat: 2},
-		{level: log.Info, text: "this is one info message", repeat: 1},
-		{level: log.Warn, text: "this is one warn message", repeat: 1},
-		{level: log.Error, text: "this is one error message", repeat: 1},
-		//{level: log.Fatal, text: "this is one fatal message", repeat: 1}, // skip this
-	}
-
-	var sb strings.Builder
-
 	for _, r := range rows {
 		for i := 0; i < r.repeat; i++ {
-			if r.level >= log.Debug {
-				sb.WriteString(fmt.Sprintf("[%s] %s\n", r.level, r.text))
-			}
+			time.Sleep(d)
+
 			switch r.level {
 			case log.Trace:
 				l.Trace(r.text)
@@ -90,8 +104,4 @@ func testWriter(t *testing.T, fp string, async bool) {
 		}
 	}
 	l.Flush()
-
-	b, err := ioutil.ReadFile(fp)
-	assert.Nil(t, err)
-	assert.Equal(t, sb.String(), string(b))
 }
